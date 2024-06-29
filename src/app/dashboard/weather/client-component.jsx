4bcde433794c6766210e5ad659bda54120354cc7
Mathieu Laurin <laurinmathieu@hotmail.com>
2024-06-29 14:14:22 -0400

    metar, taf, notam working

--- src/app/dashboard/weather/client-component.jsx
+++ src/app/dashboard/weather/client-component.jsx
@@ -11,7 +11,7 @@ export default function ClientComponent({ fetchWeather }) {
 
     const {
         airportValue, setAirportValue, // Corrected the function name here
-        weatherData, setWeatherData      
+        weatherData, setWeatherData
     } = useRccContext();
 
     const handleSubmit = async (e) => {
@@ -28,8 +28,8 @@ export default function ClientComponent({ fetchWeather }) {
 
     return (
         <>
-        <div>
-        <form onSubmit={handleSubmit}>
+            <div>
+                <form onSubmit={handleSubmit}>
                     <input
                         type="text"
                         value={airportValue.toUpperCase()}
@@ -38,23 +38,51 @@ export default function ClientComponent({ fetchWeather }) {
                     />
                     <button type="submit">Submit</button>
                 </form>
-        </div>
-        
-        <h1 className='py-5'>METAR</h1>
-        <Card title="Weather" className="bg-blue-200" style={{ width: '300px' }}>
-            <div>
-                {error && <p className='bg-orange-400' style={{ color: 'red' }}>{error}</p>}
-                {weatherData && weatherData.data && weatherData.data.length > 0 && (
-                    <div>
-                        <p>{weatherData.data[0].text}</p>
-                    </div>
-                )}
             </div>
-        </Card>
 
+            <h1 className='py-5'>METAR</h1>
+            <Card title="Weather" className="bg-blue-200" style={{ width: '300px' }}>
+                <div>
+                    {error && <p className='bg-orange-400' style={{ color: 'red' }}>{error}</p>}
+                    {weatherData && weatherData.data && weatherData.data.length > 0 && (
+                        <div>
+                            <p>{weatherData.data.find(item => item.type === 'metar').text}</p>
+                        </div>
+                    )}
+                </div>
+            </Card>
+            <h1 className='py-5'>TAF</h1>
+            <Card title="Weather" className="bg-blue-200" style={{ width: '300px' }}>
+                <div>
+                    {error && <p className='bg-orange-400' style={{ color: 'red' }}>{error}</p>}
+                    {weatherData && weatherData.data && weatherData.data.length > 0 && (
+                        <div>
+                            <p>{weatherData.data.find(item => item.type === 'taf').text}</p>
+                        </div>
+                    )}
+                </div>
+            </Card>
+            <h1 className='py-5'>NOTAM</h1>
+
+          <Card title="Weather" className="bg-blue-200" style={{ width: '300px' }}>
+    <div>
+        {error && <p className='bg-orange-400' style={{ color: 'red' }}>{error}</p>}
+        {weatherData && weatherData.data && weatherData.data.length > 0 && (
+            <div>
+                {weatherData.data.filter(item => item.type === 'notam').map((notam, index) => (
+                    <div key={index} className="mb-4"> {/* Adjust mb-4 for the desired gap between NOTAMs */}
+                        {notam.text.split('\n').map((line, lineIndex) => (
+                            <p key={lineIndex} className="mb-1">{line}</p> 
+                        ))}
+                    </div>
+                ))}
+            </div>
+        )}
+    </div>
+</Card>
 
 
         </>
-        
+
     );
 }
--- src/app/dashboard/weather/page.tsx
+++ src/app/dashboard/weather/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   const handleFetchWeather = async (location: string) => {
     
     'use server';
-    const apiUrl = `https://plan.navcanada.ca/weather/api/alpha/?site=${location}&alpha=metar&_=1719605788106`;
+    const apiUrl = `https://plan.navcanada.ca/weather/api/alpha/?site=${location}&alpha=sigmet&alpha=airmet&alpha=notam&alpha=metar&alpha=taf&alpha=pirep&notam_choice=default&_=1719683535281`;
     console.log(`API URL: ${apiUrl}`);  // Log the API URL to confirm it's correct
     const response = await fetch(apiUrl);
     const data = await response.json();
@@ -20,6 +20,10 @@ export default function Page() {
     </div>
   );
 }
+
+
+
+
 
 
 
