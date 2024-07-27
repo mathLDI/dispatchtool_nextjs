38bb9eb4619c5658c0c69f917733412c7374b712
Mathieu Laurin <laurinmathieu@hotmail.com>
2024-07-26 20:54:41 -0400

    modification to calculator look

--- src/app/lib/component/GlobalModalContent.tsx
+++ src/app/lib/component/GlobalModalContent.tsx
@@ -20,13 +20,13 @@ const GlobalModalContent: React.FC<ModalContentProps> = ({ onClose }) => {
   return (
     <>
       <Draggable disabled={disabled}>
-      <div className="flex flex-col fixed z-50 p-6 rounded-lg shadow-lg bg-white bg-opacity-90" style={{ top: '20%', left: '50%', transform: 'translate(-50%, -20%)' }}>
+      <div className="flex flex-col fixed z-50  rounded-lg shadow-lg bg-white bg-opacity-85" style={{ top: '20%', left: '50%', transform: 'translate(-50%, -20%)' }}>
       <div className="cursor-move">
           </div>
           <div>
             <SecondPageCrosswindCalculator onFocus={handleFocus} onBlur={handleBlur} />
           </div>
-          <div className="flex justify-center mt-4">
+          <div className="flex justify-center">
             <button onClick={onClose} className="px-4 py-2   text-black rounded">Close</button>
           </div>        
           </div>
