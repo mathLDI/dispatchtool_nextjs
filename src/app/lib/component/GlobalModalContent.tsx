d7eb9b1b8e0734da5b10b853f083fc39c2c3a079
Mathieu Laurin <laurinmathieu@hotmail.com>
2024-09-07 16:22:42 -0400

    modified modal so input box selection works again

--- src/app/lib/component/GlobalModalContent.tsx
+++ src/app/lib/component/GlobalModalContent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';  // Include useCallback
+import React, { useState } from 'react';
 import Draggable from 'react-draggable';
 import SecondPageCrosswindCalculator from '../../dashboard/x-wind/page';
 import FirstPageRccNotProvided from '../../dashboard/firstPageRccNotProvided/page';
@@ -6,33 +6,38 @@ import FirstPageRccProvided from '../../dashboard/firstPageRccProvided/page';
 
 interface ModalContentProps {
   onClose: () => void;
-  contentType: 'x-wind' | 'rcc-not-provided' | 'rcc-provided'; // Add a prop to decide what content to render
+  contentType: 'x-wind' | 'rcc-not-provided' | 'rcc-provided';
 }
 
 const GlobalModalContent: React.FC<ModalContentProps> = ({ onClose, contentType }) => {
   const [disabled, setDisabled] = useState(false);
 
-  const handleFocus = () => {
-    setDisabled(true);
-  };
-
-  const handleBlur = () => {
-    setDisabled(false);
-  };
+  const handleFocus = () => setDisabled(true);
+  const handleBlur = () => setDisabled(false);
 
   return (
     <>
       <Draggable disabled={disabled}>
-        <div className="flex flex-col fixed z-50 rounded-lg shadow-lg bg-white" style={{ top: '20%', left: '50%', transform: 'translate(-50%, -20%)' }}>
-          <div className="cursor-move">
+        <div
+          className="flex flex-col fixed z-50 rounded-lg shadow-lg bg-white p-4"
+          style={{ top: '20%', left: '50%', transform: 'translate(-50%, -20%)' }}
+        >
+          {/* Header area, can be used for dragging */}
+          <div className="cursor-move mb-2">
           </div>
-          <div>
-          {contentType === 'x-wind' && <SecondPageCrosswindCalculator />} 
-          {contentType === 'rcc-not-provided' && <FirstPageRccNotProvided />}
+
+          {/* Content section */}
+          <div onFocus={handleFocus} onBlur={handleBlur}>
+            {contentType === 'x-wind' && <SecondPageCrosswindCalculator />}
+            {contentType === 'rcc-not-provided' && <FirstPageRccNotProvided />}
             {contentType === 'rcc-provided' && <FirstPageRccProvided />}
           </div>
-          <div className="flex justify-center">
-            <button onClick={onClose} className="px-4 py-2 text-black rounded">Close</button>
+
+          {/* Footer with the close button */}
+          <div className="flex justify-center mt-4">
+            <button onClick={onClose} className="px-4 py-2 text-black rounded bg-gray-200">
+              Close
+            </button>
           </div>
         </div>
       </Draggable>
