cc1507ffe5a31e0b204e132d67a64dc9b06ee73e
Mathieu Laurin <laurinmathieu@hotmail.com>
2024-09-25 09:01:44 -0400

    added a server-action folder for quickSearch and weather and 'force-dynamic'

---
+++ src/app/dashboard/quickSearch/server-actions.js
@@ -0,0 +1,27 @@
+// src/app/dashboard/quickSearch/server-actions.js
+
+'use server';
+
+export const handleFetchWeather = async (location) => {
+  const apiUrl = `https://plan.navcanada.ca/weather/api/alpha/?site=${location}&alpha=notam&alpha=metar&alpha=taf&notam_choice=english&metar_choice=4&_=1719878376376`;
+  console.log(`Weather API URL: ${apiUrl}`); // Log the API URL to confirm it's correct
+
+  const response = await fetch(apiUrl, {
+    next: { revalidate: 300 }, // Revalidate every 5 minutes (300 seconds)
+  });
+
+  const data = await response.json();
+  return data;
+};
+
+export const handleFetchGFA = async (location, type) => {
+  const apiUrl = `https://plan.navcanada.ca/gfa/images/?site=${location}&image=GFA/${type}`;
+  console.log(`GFA API URL: ${apiUrl}`); // Log the API URL to confirm it's correct
+
+  const response = await fetch(apiUrl, {
+    next: { revalidate: 300 }, // Revalidate every 5 minutes (300 seconds)
+  });
+
+  const data = await response.json();
+  return data;
+};
